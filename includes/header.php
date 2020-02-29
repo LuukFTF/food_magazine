1b04796fde65471353561b136920a513f2f2a047
LuukFTF <lucasvdvegt@gmail.com>
2020-02-29 15:18:16 +0100

    intial structure

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<header class="header">
+    <nav>
+        <ul class="menu">
+            <h1 class="logo" style="font-size: 25px; color: white;">Food</h1>
+            <li class="menu-item"><a href="/">Home</a></li>
+            <li class="menu-item"><a href="#">List</a></li>
+            <li class="menu-item"><a href="#">Accounts</a></li>
+            <li class="menu-item"><a href="#">Adminpanel</a></li>
+            <li class="menu-item"><a href="#">Account Register</a></li>
+            <li class="menu-item"><a href="#">Login</a></li>
+            <li class="menu-item"><a href="#">Logout</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Luuk's Food Magazine</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+<body>
+
+<!--header-->
+<?php include 'includes/header.php'; ?>
+
+<main class="main">
+    <div class="container page">
+        <h1>Luuk's Food Magazine</h1>
+    </div>
+</main>
+
+
+
+<!--footer-->
+<?php include 'includes/footer.php'; ?>
+
+
+<!-- TODO -->
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
