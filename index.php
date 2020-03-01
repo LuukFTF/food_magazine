047a4138591c0b4d62f764a8fb7cd8aec99e6c5f
LuukFTF <lucasvdvegt@gmail.com>
2020-03-01 21:17:06 +0100

    basic block structure, main page

--- index.php
+++ index.php
@@ -13,7 +13,42 @@
 <main class="main">
     <div class="container page">
         <h1>Luuk's Food Magazine</h1>
+
+        <p></p>
+
+        <div class="item block">
+            <h1>Pizza</h1>
+            <p>Picture</p>
+            <p>Star <input type="checkbox"></p>
+            <p>Show Details</p>
+            <!--        <button type="button" class="button">Show Details</button>-->
+            <li class="button"><a href="/">Show Details</a></li>
+        </div>
+
+        <div class="item block">
+            <h1>Pizza</h1>
+            <p>Picture</p>
+            <p>Star <input type="checkbox"></p>
+            <p>Show Details</p>
+            <!--        <button type="button" class="button">Show Details</button>-->
+            <li class="button"><a href="/">Show Details</a></li>
+        </div>
+
+        <div class="detail block">
+            <h1>Food</h1>
+            <p>Starred <input type="checkbox"></p>
+            <h1>Recipe</h1>
+            <p>Stop het in de oven en gaan met die banaan.. ehh pizza!</p>
+            <h1>Tags</h1>
+            <p>kaas, oven, tomatensaus</p>
+
+        </div>
+
+
     </div>
+
+
+
 </main>
 
 
