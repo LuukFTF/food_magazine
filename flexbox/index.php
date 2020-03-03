4ae5697654a006f4340a62fb75923957734300e6
LuukFTF <lucasvdvegt@gmail.com>
2020-03-03 22:25:05 +0100

    weird flex but ok

---
+++ flexbox/index.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Luuk's Food Magazine</title>
+    <link rel="stylesheet" type="text/css" href="flexbox.css"/>
+    <script src="js/flexbox.js"></script>
+</head>
+<body>
+
+<!--header-->
+<?php //include '../includes/header.php'; ?>
+
+
+
+<main>
+    <div class="parent-main">
+        <h1>Luuk's Food Magazine</h1>
+        <div class="container">
+            <div class="item food-hover">
+                <h1>Pizza</h1>
+                <p>Picture</p>
+                <p>Star <input type="checkbox"></p>
+                <p>Show Details</p>
+                <!--        <button type="button" class="button">Show Details</button>-->
+                <li class="button"><a href="/">Show Details</a></li>
+            </div>
+            <div class="item food-hover">
+                <h1>Boerenkool</h1>
+                <p>Picture</p>
+                <p>Star <input type="checkbox"></p>
+                <p>Show Details</p>
+                <!--        <button type="button" class="button">Show Details</button>-->
+                <li class="button"><a href="/">Show Details</a></li>
+            </div>
+            <div class="item special-item food-hover">
+                <h1>Lasagna</h1>
+                <p>Picture</p>
+                <p>Star <input type="checkbox"></p>
+                <p>Show Details</p>
+                <!--        <button type="button" class="button">Show Details</button>-->
+                <li class="button"><a href="/">Show Details</a></li>
+            </div>
+            <div class="item food-hover">
+                <h1>Kebab</h1>
+                <p>Picture</p>
+                <p>Star <input type="checkbox"></p>
+                <p>Show Details</p>
+                <!--        <button type="button" class="button">Show Details</button>-->
+                <li class="button"><a href="/">Show Details</a></li>
+            </div>
+            <div class="item food-hover">
+                <h1>Paella</h1>
+                <p>Picture</p>
+                <p>Star <input type="checkbox"></p>
+                <p>Show Details</p>
+                <!--        <button type="button" class="button">Show Details</button>-->
+                <li class="button"><a href="/">Show Details</a></li>
+            </div>
+            <div class="item food-hover">
+                <h1>Patat</h1>
+                <p>Picture</p>
+                <p>Star <input type="checkbox"></p>
+                <p>Show Details</p>
+                <!--        <button type="button" class="button">Show Details</button>-->
+                <li class="button"><a href="/">Show Details</a></li>
+            </div>
+            <div class="item food-hover">
+                <h1>Babi Panggang</h1>
+                <p>Picture</p>
+                <p>Star <input type="checkbox"></p>
+                <p>Show Details</p>
+                <!--        <button type="button" class="button">Show Details</button>-->
+                <li class="button"><a href="/">Show Details</a></li>
+            </div>
+
+        </div>
+    </div>
+</main>
+
+
+
+
+<!--footer-->
+<?php //include '../includes/footer.php'; ?>
+
+
+<!-- TODO -->
+
+</body>
+</html>
